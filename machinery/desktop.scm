9b1db01a0205b4156120a4d15018d214bc87dcc2
uyotew <uyotew@nomail>
2025-03-25 16:38:07 +0100

    added blender and steam to desktop

--- machinery/desktop.scm
+++ machinery/desktop.scm
@@ -1,9 +1,16 @@
 (define-module (machinery desktop)
   #:use-module (gnu)
+  #:use-module (gnu home)
+  #:use-module (gnu packages graphics)
+  #:use-module (nongnu packages game-client)
   #:use-module (machinery common)
   #:export (desktop-home desktop-system))
 
-(define desktop-home base-home)
+(define desktop-home
+  (home-environment
+    (inherit base-home)
+    (packages (cons* steam blender
+                     (home-environment-packages base-home)))))
 
 (define (desktop-system keypit)
   (operating-system
