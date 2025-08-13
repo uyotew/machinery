b3074d86e80fc057d27ea01d95ec32851b6ba188
uyotew <uyotew@nomail>
2025-08-13 02:14:03 +0200

    prompt-color added as parameter to base-home

--- machinery/laptop.scm
+++ machinery/laptop.scm
@@ -9,19 +9,20 @@
   #:export (laptop-home laptop-system))
 
 (define laptop-home
-  (home-environment
-    (inherit base-home)
-    (services (cons*
-     (simple-service 'exec-sway home-bash-service-type
-      (home-bash-extension
-       (bash-profile
-        (list
-         (plain-file "exec-sway" "\
+  (let ((base (base-home)))
+    (home-environment
+      (inherit base)
+      (services (cons*
+       (simple-service 'exec-sway home-bash-service-type
+        (home-bash-extension
+         (bash-profile
+          (list
+           (plain-file "exec-sway" "\
 if [ -z \"$WAYLAND_DISPLAY\" ] && [ -n \"$XDG_VTNR\" ] && [ \"$XDG_VTNR\" -eq 1 ] ; then
     exec sway
 fi
 ")))))
-     (home-environment-user-services base-home)))))
+       (home-environment-user-services base))))))
 
 (define (laptop-system keypit)
   (let ((base (base-system "laptop" "EF1A-92A8" "10.4.4.2/24" laptop-home keypit)))
