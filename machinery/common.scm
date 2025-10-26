f96a2201ac29b07c303eae23c7dcb47c870fd676
uyotew <uyotew@nomail>
2025-10-26 12:48:39 +0100

    sway swaybg: kill old instances when restarting

--- machinery/common.scm
+++ machinery/common.scm
@@ -143,7 +143,7 @@ root ALL=(ALL) ALL
     ;; Allow resolution of '.local' host names with mDNS.
     (name-service-switch %mdns-host-lookup-nss)))
 
-(define* (base-home #:optional (theme %default-light-theme))
+(define* (base-home #:optional (theme %default-dark-theme))
  (home-environment
   (packages (list 
              zig-0.15 git openssh wireplumber bluez
