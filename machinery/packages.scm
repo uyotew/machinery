58ce00e91c0f31172fd5909f1491669f3bd70f0b
uyotew <uyotew@nomail>
2025-10-28 19:35:10 +0100

    statusbar: update to work with zig 0.15.2

--- machinery/packages.scm
+++ machinery/packages.scm
@@ -27,8 +27,8 @@
   (license license:expat)))
 
 (define-public statusbar
- (let ((commit "6a03952140322ba188209f51f3a4eb0f8af2ea0d")
-        (revision "1"))
+ (let ((commit "79fa70ac92a62e13b002ecee680a801f87a23f40")
+        (revision "2"))
   (package
    (name "statusbar")
    (version (git-version "1.0" revision commit))
@@ -40,7 +40,7 @@
            (commit commit)))
      (file-name (git-file-name name version))
      (sha256
-      (base32 "0wwbrsc1dh95z0mbk8wgs73vzi9hfkbw435qd44mwb77jid40hyn"))))
+      (base32 "1hdrikqhjyhy06lcpa7c5gz284d7j5nidma334bhi55v7ysv8ysd"))))
    (build-system zig-build-system)
    (arguments (list
                #:tests? #f
